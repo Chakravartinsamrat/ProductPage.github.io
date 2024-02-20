ab334023aba076db9e8f3a8039dc4c0c847aca4c
Piyush Chakarborthy <102757301+Chakravartinsamrat@users.noreply.github.com>
2024-02-20 19:56:08 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero';
+import About from './components/About';
+import Support from './components/Support';
+import Pricing from './components/Pricing';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <About/>
+    <Support/>
+    <Pricing/>
+    <Footer/>
     </>
   );
 }
